bc122e179e35011410e657a6ffcc9ec9440df647
Slava <svjatoslavs.krasnikovs@gmail.com>
2021-01-13 07:43:05 +0200

    Initial commit

---
+++ src/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\GeneratedValue;
+
+/**
+ * @Entity @Table(name="products")
+ */
+class Product
+{
+    /** @Id @Column(type="integer") @GeneratedValue */
+    protected $id;
+    /** @Column(type="string") */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
